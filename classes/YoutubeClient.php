bf7e0922faff305f5812743900a29651fb797d49
Yacine REZGUI <yacine.rezgui@gmail.com>
2019-08-18 10:17:21 +0200

    Fix Error : get file url content
    Fix Error : get url content caused if can't get content or youtube quota execed

--- classes/YoutubeClient.php
+++ classes/YoutubeClient.php
@@ -7,30 +7,59 @@ class YoutubeClient
 {
     use Singleton;
 
-    private $obj;
+	private $obj;	
 	private $key;
-    private $channel;
+	private $channel;
+	private $url;
     
     public function __construct()
 	{
-        $settings = Settings::instance();
-        $this->key = $settings->api_key;
+		$settings = Settings::instance();
+		$this->key = $settings->api_key;
 		$this->channel = $settings->channel_id;
-		$url = "https://www.googleapis.com/youtube/v3/search?key=$this->key&channelId=$this->channel&part=snippet,id&order=date&maxResults=9";
-		$json = file_get_contents($url);
+		$this->url = "https://www.googleapis.com/youtube/v3/search?key=$this->key&channelId=$this->channel&part=snippet,id&order=date";
+		$url = $this->url."&maxResults=9&type=video";
+		$json = $this->file_get_contents_curl($url);
 		$this->obj = json_decode($json, true);
 	}
 	public function getVideosPortada($cnt)
 	{
-		$url = "https://www.googleapis.com/youtube/v3/search?key=$this->key&channelId=$this->channel&part=snippet,id&order=date&maxResults=$cnt&type=video";
-		$json = file_get_contents($url);
+		$url = $this->url."&maxResults=".$cnt."&type=video";
+		$json = $this->file_get_contents_curl($url);
 		$this->obj = json_decode($json, true);
-        return $this->obj['items'];
+
+		return (isset($this->obj['items'])) ? $this->obj['items'] : null;
 	}
 	public function getVideos()
 	{
-        $videos = $this->obj['items'];
-		return $videos;
+		return (isset($this->obj['items'])) ? $this->obj['items'] : null;
 	}
+
+	private function file_get_contents_curl($url) {
+
+		$handle= curl_init();
+		curl_setopt_array($handle, array(
+      					CURLOPT_URL            => $url,
+      					CURLOPT_RETURNTRANSFER => true,
+					CURLOPT_HEADER         => false
+					)
+		);
+
+		$data = curl_exec($handle);
+		
+		$responseCode   = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+		$downloadLength = curl_getinfo($handle, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+ 
+		if(curl_errno($handle))
+		{
+			return null;
+		} else
+		{
+			//if($responseCode == "200") echo "successful request";
+			curl_close($handle);
+			return $data;
+		}
+	}
+	
 }
 
